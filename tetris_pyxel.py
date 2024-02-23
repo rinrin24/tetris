f17fc4df281148c496ec53d3c3eb976c96b00f8f
rinrin24 <rin.nkmr42@gmail.com>
2024-02-23 21:50:03 +0900

    feat: drop method

--- tetris_pyxel.py
+++ tetris_pyxel.py
@@ -1,7 +1,29 @@
 import copy
 import pyxel
+from dataclasses import dataclass, field
 from typing import ClassVar
 from main import Tetris, Position, EmptyMino
+
+@dataclass(frozen=True, slots=True)
+class DropTimer:
+    DEFAULT_DROP_PERIOD: ClassVar[int] = 60
+    DEFAULT_DROP_SPEED: ClassVar[int] = 1
+    drop_period: int = field(init=False, default_factory=int)
+    timer: int = field(default=0)
+    _speed: int = field(default=DEFAULT_DROP_SPEED)
+    def __post_init__(self):
+        object.__setattr__(self, "drop_period", DropTimer.DEFAULT_DROP_PERIOD/self._speed)
+    def should_drop(self):
+        return self.timer >= self.drop_period
+
+@dataclass(frozen=True)
+class DropDelay:
+    MAX_TIME: ClassVar[int] = 30
+    MAX_COUNT: ClassVar[int] = 8
+    timer: int = field(default=0)
+    reset_count: int = field(default=0)
+    def should_place(self) -> bool:
+        return (self.timer >= DropDelay.MAX_TIME) or (self.reset_count >= DropDelay.MAX_COUNT)
 
 class App:
     NEXT_NUMBER: ClassVar[int] = 5
@@ -10,6 +32,9 @@ class App:
         pyxel.load('my_resource.pyxres')
         self.tetris = Tetris()
         self.tetris.make_mino()
+        self.speed = DropTimer.DEFAULT_DROP_SPEED
+        self.drop_timer = DropTimer(_speed=self.speed)
+        self.delay = DropDelay()
         pyxel.run(self.update, self.draw) # アプリケーションの実行
 
     def update(self): # フレームの更新処理
@@ -27,6 +52,29 @@ class App:
             self.tetris.hold()
         if pyxel.btnp(pyxel.KEY_SPACE):
             self.tetris.hard_drop()
+            self.drop_timer = DropTimer(_speed=self.speed)
+        self.drop()
+
+    def drop(self):
+        if self.tetris.is_bottom():
+            if self.delay.should_place():
+                self.tetris.place_mino()
+                self.drop_timer = DropTimer(_speed=self.speed)
+                self.delay = DropDelay()
+            else:
+                self.delay = DropDelay(timer=self.delay.timer+1, reset_count=self.delay.reset_count)
+        if not self.tetris.is_bottom():
+            if self.delay.timer > 0:
+                self.delay = DropDelay(reset_count=self.delay.reset_count+1)
+                self.drop_timer = DropTimer(_speed=self.speed)
+        if self.drop_timer.should_drop():
+            self.drop_timer = DropTimer(_speed=self.speed)
+            if self.tetris.is_bottom():
+                self.tetris.place_mino()
+            else:
+                self.tetris.move_down()
+        else:
+            self.drop_timer = DropTimer(self.drop_timer.timer+1, self.speed)
 
     def draw(self): # 描画処理
         pyxel.cls(0)
@@ -45,7 +93,6 @@ class App:
             for x, block in enumerate(column):
                 if not block.is_empty():
                     new_grid.add_block(Position(mino_position_x+x, mino_position_y+y), block)
-        print(new_grid)
         for y, column in enumerate(reversed(new_grid.grid)):
             if y <= 19:
                 continue
